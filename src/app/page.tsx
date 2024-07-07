144ee5bb2d41f5f6af4066bf4ae7cf4d68b95b0c
MrBeeni <mubeen4429@gmail.com>
2024-07-07 22:51:53 +0500

    app setup complete

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
