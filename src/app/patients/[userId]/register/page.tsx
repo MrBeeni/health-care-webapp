e9b6b30acb429b3c9939ea44656ed4e8c7ac22a6
MrBeeni <mubeen4429@gmail.com>
2024-07-10 12:55:33 +0500

    feat: design appointment page | implemented functionality to save appointment

--- src/app/patients/[userId]/register/page.tsx
+++ src/app/patients/[userId]/register/page.tsx
@@ -1,16 +1,12 @@
-import { getUser } from "@/actions/patient.actions";
+import { getPatient, getUser } from "@/actions/patient.actions";
 import RegisterForm from "@/components/forms/RegisterForm";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
-// import RegisterForm from "@/components/forms/RegisterForm";
-// import { getPatient, getUser } from "@/lib/actions/patient.actions";
-
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
-  // const patient = await getPatient(userId);
-
-  // if (patient) redirect(`/patients/${userId}/new-appointment`);
+  const patient = await getPatient(userId);
+  if (patient) redirect(`/patients/${userId}/new-appointment`);
 
   return (
     <div className="flex h-screen max-h-screen">
